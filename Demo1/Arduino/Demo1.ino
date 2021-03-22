58579fa556b95d278537d230e431a9ec9a038f2e
wpeyton <55258143+wpeyton@users.noreply.github.com>
2021-03-22 12:26:36 -0600

    Update Demo1.ino

--- Demo1/Arduino/Demo1.ino
+++ Demo1/Arduino/Demo1.ino
@@ -2,9 +2,9 @@
 
 #define COUNTS_PER_REVOLUTION 3200  // Number of counts per revolution on the encoder on the motor
 
-#define NUM_FT_TO_MOVE 1
-#define NUM_DEG_TO_TURN 0
-#define TRANS_RAMP_UP_TIME 107
+#define NUM_FT_TO_MOVE 1            // Number of feet to move forward
+#define NUM_DEG_TO_TURN 0           // Number of degrees to rotate
+#define TRANS_RAMP_UP_TIME 107      // Extra time for linear motion to account for motor ramp up time
 
 #define M_ENABLE  4                 // Motor enable pin -- must be set high to operate motors
 #define M1_DIR    7                 // Motor 1 direction pin -- set true for cw rotation and false for ccw
@@ -21,43 +21,43 @@ int  M1_PWM_Val = 0;                // Value to store speed of motor 1 -- set va
 int  M2_PWM_Val = 0;                // Value to store speed of motor 2 -- set value between 0 and 255 (0 to not move, 255 is max speed)
 
 // Vars for all controllers
-const float r = 7.5 * 0.01;
-const float d = 24.62 * 0.01;
+const float r = 7.5 * 0.01;         // Radius of the wheel
+const float d = 24.62 * 0.01;       // Distance between the wheels
 
-float Va = 0;
-float delta_Va = 0;
+float Va = 0;                       // Control algorithm output, between [-1.0, 1.0], describes linear motion     
+float delta_Va = 0;                 // Control algorithm output, between [-1.0, 1.0], describes rotational motio
 
-float theta_1_previous = 0;
-float theta_2_previous = 0;
+float theta_1_previous = 0;         // Store the previous value to determine the derivative terms
+float theta_2_previous = 0;         // Store the previous value to determine the derivative terms
 
-float theta_1 = 0;
-float theta_2 = 0;
+float theta_1 = 0;                  // Store the current position of wheel 1 in radians
+float theta_2 = 0;                  // Store the current position of wheel 2 in radians
 
-float theta_dot_1 = 0;
-float theta_dot_2 = 0;
+float theta_dot_1 = 0;              // Store the current velocity of wheel 1 in radians per second 
+float theta_dot_2 = 0;              // Store the current velocity of wheel 2 in radians per second 
 
 // Vars for rot_pos_controller
-float phi_set = 0.0;
+float phi_set = 0.0;                //  Variable storing desired position in radians
 
-float phi = 0;
-float phi_error = 0;
-float phi_error_prev = 0;
-float phi_error_dot = 0;
-float phi_total_error = 0;
+float phi = 0;                      // Current Position
+float phi_error = 0;                // Positional error
+float phi_error_prev = 0;           // Last positional error
+float phi_error_dot = 0;            // Derivative of positional error
+float phi_total_error = 0;          // Integral of positional error
 
-const float Kp_phi = 10;
-const float Ki_phi = 10;
-const float Kd_phi = 0.25;
+const float Kp_phi = 10;            // Positional controller proportional gain 
+const float Ki_phi = 10;            // Positional controller integral gain
+const float Kd_phi = 0.25;          // Positional controller derivative gain
 
 // Vars for rot_speed_controller
-float phi_dot_set = 0.0;
+float phi_dot_set = 0.0;            // Variable storing desired angular velocity in radians per second
 
-float phi_dot = 0;
-float phi_dot_error = 0;
-float phi_dot_total_error = 0;
+float phi_dot = 0;                  // Current angular velocity
+float phi_dot_error = 0;            // Angular velocity error
+float phi_dot_total_error = 0;      // Integral of angular velocity error
 
-const float Kp_phi_dot = 0.1;
-const float Ki_phi_dot = 1;
+const float Kp_phi_dot = 0.1;       // Velocity controller proportional gain
+const float Ki_phi_dot = 1;         // Velocity controller proportional gain
 
 // Vars for trans_speed_controller
 float rho_dot_set = 0.0;
@@ -80,68 +80,65 @@ void setup() {
   pinMode(M2_DIR, OUTPUT);          // Define direction pin as output
   pinMode(M2_PWM, OUTPUT);          // Define pwm pin as output
 
-  Serial.begin(9600);
-  while (!Serial);
+  Serial.begin(9600);               // Initialize Serial comms
+  while (!Serial);                  // Wait for initialization
 
   digitalWrite(M_ENABLE, HIGH);     // IMPORTANT!! -- set enable pin high
-  digitalWrite(M1_DIR, true); // Set initial motor direction to cw
-  analogWrite(M1_PWM, 0);  // Set initial motor speed to 0
-  digitalWrite(M2_DIR, true); // Set initial motor direction to cw
-  analogWrite(M2_PWM, 0);  // Set initial motor speed to 0
+  digitalWrite(M1_DIR, true);       // Set initial motor direction to cw
+  analogWrite(M1_PWM, 0);           // Set initial motor speed to 0
+  digitalWrite(M2_DIR, true);       // Set initial motor direction to cw
+  analogWrite(M2_PWM, 0);           // Set initial motor speed to 0
   
-  OCR0A = 0xAF;
-  TIMSK0 |= _BV(OCIE0A);
+  OCR0A = 0xAF;                     // Set TMR0 interrupt comparison value
+  TIMSK0 |= _BV(OCIE0A);            // Enable TMR0 interrupt
 
-  startTime = millis();
-  phi_set = NUM_DEG_TO_TURN * PI / 180;
+  startTime = millis();                 // Store program start time
+  phi_set = NUM_DEG_TO_TURN * PI / 180; // convert positional set pt from degrees to radians
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Record current time
   currentTime = millis();
   
+  // Set motor ouputs to the calculated values
   digitalWrite(M1_DIR, M1_Dir_Val);
   analogWrite(M1_PWM, M1_PWM_Val);
   digitalWrite(M2_DIR, M2_Dir_Val);
   analogWrite(M2_PWM, M2_PWM_Val);
 
+  // If you have already turned, go forward X ft
+  // If you havent turned yet, turn
   if(currentTime - startTime >= ((NUM_FT_TO_MOVE * 0.3048 / 0.5) * 1000)+ TRANS_RAMP_UP_TIME + (12 * abs(NUM_DEG_TO_TURN))) {
     rho_dot_set = 0.0;
   } else if(currentTime - startTime >= 12 * abs(NUM_DEG_TO_TURN)) {
     rho_dot_set = 0.5;
   }
-  
-//  delay(25);
-//  Serial.print(phi);
-//  Serial.print("\t");
-//  Serial.print(phi_dot_set);
-//  Serial.print("\t");
-//  Serial.print(M1_PWM_Val);
-//  Serial.print("\t");
-//  Serial.println(M2_PWM_Val);
 }
 
+// TMR0 interrupt service routine
 SIGNAL(TIMER0_COMPA_vect) {
-  readEncoders();
-  phi_controller();
-  phi_dot_controller();
-  rho_dot_controller();
-  setMotorVals();
+  readEncoders();       // Get current position
+  phi_controller();     // Calculate necessary angular speed
+  phi_dot_controller(); // Calculate deltaVa
+  rho_dot_controller(); // Calcualte Va
+  setMotorVals();       // Scale ouput for PWM
 }
 
+// Record current position and velocuty of the wheels
 void readEncoders() {
-  theta_1_previous = theta_1;
-  theta_2_previous = theta_2;
+  theta_1_previous = theta_1;   // Store previous value 
+  theta_2_previous = theta_2;   // Store previous value 
 
-  theta_1 = -2.0 * PI * enc1.read() / COUNTS_PER_REVOLUTION;  // Convert counts to radians
-  theta_2 = 2.0 * PI * enc2.read() / COUNTS_PER_REVOLUTION;  // Convert counts to radians
+  theta_1 = -2.0 * PI * enc1.read() / COUNTS_PER_REVOLUTION;  // Store position in radians
+  theta_2 = 2.0 * PI * enc2.read() / COUNTS_PER_REVOLUTION;   // Store position in radians
 
-  theta_dot_1 = (theta_1 - theta_1_previous) * 1000;
-  theta_dot_2 = (theta_2 - theta_2_previous) * 1000;
+  theta_dot_1 = (theta_1 - theta_1_previous) * 1000;          // Calculate angular velocity
+  theta_dot_2 = (theta_2 - theta_2_previous) * 1000;          // Calculate angular velocity
 }
 
+// Apply feedback control to calculate necessary angular velocity
 void phi_controller() {
-  phi = - r * (theta_1 - theta_2) / d;
+  phi = - r * (theta_1 - theta_2) / d;  // Calculate current angle
   phi_error_prev = phi_error;
   phi_error = phi_set - phi;
 
