c4c2ac8763a0419a58c9d838dee4b85fec9f339b
wpeyton <55258143+wpeyton@users.noreply.github.com>
2021-03-22 12:06:37 -0600

    Update rot_pos_controller.ino

--- Demo1/Arduino/rot_pos_controller.ino
+++ Demo1/Arduino/rot_pos_controller.ino
@@ -1,3 +1,13 @@
+/*  Group 10 - Andrew Burton, Trevor Bachand, William Peyton, & Kyra Squier
+    EENG350B - SEED Lab
+    Rotational Position Controller Arduino Code
+    05 March 2021
+    Description:
+    The following program runs the rotational position feedback control algorithm. 
+    A positional set point is stored as a variable, and the motor outputs are calculated to 
+    get the robot to that position. 
+*/
+
 #include <Encoder.h>                // Encoder Library
 
 #define COUNTS_PER_REVOLUTION 3200  // Number of counts per revolution on the encoder on the motor
@@ -17,43 +27,43 @@ int  M1_PWM_Val = 0;                // Value to store speed of motor 1 -- set va
 int  M2_PWM_Val = 0;                // Value to store speed of motor 2 -- set value between 0 and 255 (0 to not move, 255 is max speed)
 
 // Vars for all controllers
-const float r = 7.5 * 0.01;
-const float d = 25 * 0.01;
+const float r = 7.5 * 0.01;         // Radius of the wheel
+const float d = 25 * 0.01;          // Distance between the wheels
 
-float Va = 0;
-float delta_Va = 0;
+float Va = 0;                       // Control algorithm output, between [-1.0, 1.0], describes linear motion
+float delta_Va = 0;                 // Control algorithm output, between [-1.0, 1.0], describes rotational motion
 
-float theta_1_previous = 0;
-float theta_2_previous = 0;
+float theta_1_previous = 0;         // Store the previous value to determine the derivative terms
+float theta_2_previous = 0;         // Store the previous value to determine the derivative terms
 
-float theta_1 = 0;
-float theta_2 = 0;
+float theta_1 = 0;                  // Store the current position of wheel 1 in radians 
+float theta_2 = 0;                  // Store the current position of wheel 2 in radians
 
-float theta_dot_1 = 0;
-float theta_dot_2 = 0;
+float theta_dot_1 = 0;              //  Store the current velocity of wheel 1 in radians per second  
+float theta_dot_2 = 0;              //  Store the current velocity of wheel 2 in radians per second
 
 // Vars for rot_pos_controller
-float phi_set = PI / 2;
+float phi_set = PI / 2;             //  Variable storing desired position in radians
 
-float phi = 0;
-float phi_error = 0;
-float phi_error_prev = 0;
-float phi_error_dot = 0;
-float phi_total_error = 0;
+float phi = 0;                      // Current Position
+float phi_error = 0;                // Positional error
+float phi_error_prev = 0;           // Last positional error
+float phi_error_dot = 0;            // Derivative of positional error
+float phi_total_error = 0;          // Integral of positional error
 
-const float Kp_phi = 10;
-const float Ki_phi = 10;
-const float Kd_phi = 0.25;
+const float Kp_phi = 10;            // Positional controller proportional gain    
+const float Ki_phi = 10;            // Positional controller integral gain
+const float Kd_phi = 0.25;          // Positional controller derivative gain
 
 // Vars for rot_speed_controller
-float phi_dot_set = 0.0;
+float phi_dot_set = 0.0;            // Variable storing desired angular velocity in radians per second
 
-float phi_dot = 0;
-float phi_dot_error = 0;
-float phi_dot_total_error = 0;
+float phi_dot = 0;                  // Current angular velocity
+float phi_dot_error = 0;            // Angular velocity error
+float phi_dot_total_error = 0;      // Integral of angular velocity error
 
-const float Kp_phi_dot = 0.1;
-const float Ki_phi_dot = 1;
+const float Kp_phi_dot = 0.1;       // Velocity controller proportional gain
+const float Ki_phi_dot = 1;         // Velocity controller proportional gain
 
 void setup() {
   // put your setup code here, to run once:
@@ -63,27 +73,29 @@ void setup() {
   pinMode(M2_DIR, OUTPUT);          // Define direction pin as output
   pinMode(M2_PWM, OUTPUT);          // Define pwm pin as output
 
-  Serial.begin(9600);
-  while (!Serial);
+  Serial.begin(9600);               // Initialize Serial comms
+  while (!Serial);                  // Wait for initialization
 
   digitalWrite(M_ENABLE, HIGH);     // IMPORTANT!! -- set enable pin high
-  digitalWrite(M1_DIR, true); // Set initial motor direction to cw
-  analogWrite(M1_PWM, 0);  // Set initial motor speed to 0
-  digitalWrite(M2_DIR, true); // Set initial motor direction to cw
-  analogWrite(M2_PWM, 0);  // Set initial motor speed to 0
+  digitalWrite(M1_DIR, true);       // Set initial motor direction to cw
+  analogWrite(M1_PWM, 0);           // Set initial motor speed to 0
+  digitalWrite(M2_DIR, true);       // Set initial motor direction to cw
+  analogWrite(M2_PWM, 0);           // Set initial motor speed to 0
 
-  OCR0A = 0xAF;
-  TIMSK0 |= _BV(OCIE0A);
+  OCR0A = 0xAF;                     // Set TMR0 interrupt comparison value
+  TIMSK0 |= _BV(OCIE0A);            // Enable TMR0 interrupt
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Set motor ouputs to the calculated values
   digitalWrite(M1_DIR, M1_Dir_Val);
   analogWrite(M1_PWM, M1_PWM_Val);
   digitalWrite(M2_DIR, M2_Dir_Val);
   analogWrite(M2_PWM, M2_PWM_Val);
 
-  delay(25);
+  delay(25); // Wait 25 ms
+  
+  // Print out debug info to serial monitor
   Serial.print(phi);
   Serial.print("\t");
   Serial.print(phi_dot_set);
@@ -93,70 +105,84 @@ void loop() {
   Serial.println(M2_PWM_Val);
 }
 
+// TMR0 interrupt service routine
 SIGNAL(TIMER0_COMPA_vect) {
-  readEncoders();
-  phi_controller();
-  phi_dot_controller();
-  setMotorVals();
+  readEncoders();       // Get current position
+  phi_controller();     // Calculate necessary angular speed
+  phi_dot_controller(); // Calculate deltaVa
+  setMotorVals();       // Scale ouput for PWM
 }
 
 void readEncoders() {
-  theta_1_previous = theta_1;
-  theta_2_previous = theta_2;
+  theta_1_previous = theta_1;   // Store previous value 
+  theta_2_previous = theta_2;   // Store previous value
 
-  theta_1 = -2.0 * PI * enc1.read() / COUNTS_PER_REVOLUTION;  // Convert counts to radians
-  theta_2 = 2.0 * PI * enc2.read() / COUNTS_PER_REVOLUTION;  // Convert counts to radians
+  theta_1 = -2.0 * PI * enc1.read() / COUNTS_PER_REVOLUTION;  // Store position in radians
+  theta_2 = 2.0 * PI * enc2.read() / COUNTS_PER_REVOLUTION;   // Store position in radians
 
-  theta_dot_1 = (theta_1 - theta_1_previous) * 1000;
-  theta_dot_2 = (theta_2 - theta_2_previous) * 1000;
+  theta_dot_1 = (theta_1 - theta_1_previous) * 1000;          // Calculate angular velocity
+  theta_dot_2 = (theta_2 - theta_2_previous) * 1000;          // Calculate angular velocity
 }
 
+// Apply feedback control to calculate necessary angular velocity
 void phi_controller() {
-  phi = - r * (theta_1 - theta_2) / d;
-  phi_error_prev = phi_error;
-  phi_error = phi_set - phi;
+  phi = - r * (theta_1 - theta_2) / d;      // Calculate current angle
+  phi_error_prev = phi_error;               // Store prev error
+  phi_error = phi_set - phi;                // Calculate current error
 
+  // Error bounding
   if (phi_error < -1.0) phi_error = -1.0;
   if (phi_error > 1.0) phi_error = 1.0;
 
+  // Calculate integral of the error, apply integral clamping
   if (abs(phi_total_error) < 0.5 || (phi_error < 0 && phi_total_error > 0) || (phi_error > 0 && phi_total_error < 0)) {
     phi_total_error += (phi_error * 0.001); // Increment the integral of the error
   }
-
+  
+  // Calculate derivative of the error
   phi_error_dot = (phi_error - phi_error_prev) * 1000;
   
+  // Apply PID control algorithm to determine angular velocity
   phi_dot_set = Kp_phi * phi_error + Kd_phi * phi_error_dot;
-  // Ki_phi * phi_total_error
   
+  // Output bounding
   if (phi_dot_set > 10.0) phi_dot_set = 10.0;
   if (phi_dot_set < -10.0) phi_dot_set = -10.0;
 
 }
 
+// Apply feedback control to calculate motor voltages to rotate as desired
 void phi_dot_controller() {
-  phi_dot = - r * (theta_dot_1 - theta_dot_2) / d;
-  phi_dot_error = phi_dot_set - phi_dot;
+  phi_dot = - r * (theta_dot_1 - theta_dot_2) / d;    // Calculate angular velocity
+  phi_dot_error = phi_dot_set - phi_dot;              // Calculate angular velocity error
 
-  if (phi_dot_error < -10.0) phi_dot_error = -10.0;
+  // Error bounding
+  if (phi_dot_error < -10.0) phi_dot_error = -10.0; 
   if (phi_dot_error > 10.0) phi_dot_error = 10.0;
 
+  // Update the integral term, apply integral clamping
   if (abs(phi_dot_total_error) < 0.5 || (phi_dot_error < 0 && phi_dot_total_error > 0) || (phi_dot_error > 0 && phi_dot_total_error < 0)) {
     phi_dot_total_error += (phi_dot_error * 0.001); // Increment the integral of the error
   }
-
+  
+  // Apply feedback control alorithm to calculate deltaVa
   delta_Va = - (Kp_phi_dot * phi_dot_error + Ki_phi_dot * phi_dot_total_error);
 }
 
+// Scale the controller outputs to be pwm values
 void setMotorVals() {
+  // Calculate the pwm value for each motor based on Va, deltaVa
   M1_PWM_Val = (int) (255 * (Va + delta_Va) / 2.0);
   M2_PWM_Val = (int) (255 * (Va - delta_Va) / 2.0);
 
+  // Set motor directions
   if (M1_PWM_Val > 0) M1_Dir_Val = false;
   else M1_Dir_Val = true;
 
   if (M2_PWM_Val > 0) M2_Dir_Val = true;
   else M2_Dir_Val = false;
-
+  
+  // Make PWM output unsigned
   M1_PWM_Val = abs(M1_PWM_Val);
   M2_PWM_Val = abs(M2_PWM_Val);
 }
